7c545075650ad415e44dc72bb2ae54d3cf68031c
gk42111 <56386645+gk42111@users.noreply.github.com>
2020-04-30 16:37:06 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "Punkt.h"
 #include "Tablice.h"
 #include "TabDyn.h"
+#include "Vector.h"
+#include "Student.h"
 using namespace std;
 int main() {
 	//Zad1
@@ -19,6 +21,28 @@ int main() {
 	cout << tablice1.Max() << " " << tablice1.Min() << endl;
 	cout << tablice2.Suma() << endl;
 
+	//Zad3
+	TabDyn tab(10);
+	tab.GetElement(9);
+	tab.SetElement(8);
+	cout << tab.Sum() << endl;
+	cout << tab.Mean() << endl;
+
+	//Zad4
+	Vector v(1, 2), v1(3,4);
+	cout << v.dodawanie(v1)<< endl;
+	cout << v.odejmowanie(v1) << endl;
+	cout << v.mnozenie(v1) << endl;
+	cout << v.dzielenie(v1) << endl;
+	
+	//Zad5
+	Student s1("Jan", "Kowalski", 25);
+	Student s2("Adam", "Nowak", 20);
+	cout << s1.CzyZdal() << endl;
+	cout << s2.CzyZdal() << endl;
+	s1.DaneStudenta();
+	s2.DaneStudenta();
+	
 	system("pause");
 	return 0;
 }
